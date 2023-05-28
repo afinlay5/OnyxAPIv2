bdf903a870db9045ee39e302873aba6fa1fed1f5
Adrian D Finlay <afinlay@live.com>
2023-05-28 02:11:26 -0400

    Dev/Interim - Disable Spring Data for now, Suppress warnings

--- src/test/java/com/onyx/onyxapi/it/OnyxApiApplicationTests.java
+++ src/test/java/com/onyx/onyxapi/it/OnyxApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.onyx.onyxapi;
+package com.onyx.onyxapi.it;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OnyxApiApplicationTests {
 
+	@SuppressWarnings("java:S2699")
 	@Test
 	void contextLoads() {
 	}
