93e6f150fd93d80598ba84f7d5c3d9380d9d74ca
Adrian D Finlay <afinlay@live.com>
2023-06-05 00:42:08 -0400

    Dev/Interim - Looking good

--- onyx-api/src/main/java/com/onyx/api/intercept/config/RESTApiConfigurer.java
+++ onyx-api/src/main/java/com/onyx/api/intercept/config/RESTApiConfigurer.java
@@ -1,13 +1,24 @@
 package com.onyx.api.intercept.config;
 
 import com.onyx.api.intercept.DynamicDataSourceRoutingInterceptor;
+import com.onyx.commons.model.BasketballPlayerStatisticsDataStore;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
 
+import javax.sql.DataSource;
+import java.util.Map;
+
 @Configuration
 public class RESTApiConfigurer implements WebMvcConfigurer {
+
+    private final Map<BasketballPlayerStatisticsDataStore, DataSource> dataStoreMap;
+
+    public RESTApiConfigurer(@Qualifier Map<BasketballPlayerStatisticsDataStore, DataSource> dataStoreMap) {
+        this.dataStoreMap = dataStoreMap;
+    }
 
     /**
      * Add Spring MVC lifecycle interceptors for pre- and post-processing of
@@ -21,6 +32,6 @@ public class RESTApiConfigurer implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(
                 new WebRequestHandlerInterceptorAdapter(
-                        new DynamicDataSourceRoutingInterceptor(java.util.Map.of())));
+                        new DynamicDataSourceRoutingInterceptor(dataStoreMap)));
     }
 }
